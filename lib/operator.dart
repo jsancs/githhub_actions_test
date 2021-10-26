bc63d96ae20246d160bb4052e6b4cb0a5f6bc1b7
jsancs05 <javivisan19@gmail.com>
2021-10-26 11:37:34 +0200

    Added sub method

--- lib/operator.dart
+++ lib/operator.dart
@@ -2,7 +2,7 @@ class Operator {
   Operator();
 
   int sum(int a, int b) => a + b;
-  //int sub(int a, int b) => a - b;
+  int sub(int a, int b) => a - b;
   //int mul(int a, int b) => a * b;
   //int mod(int a, int b) => a % b;
 }
