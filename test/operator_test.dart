1c9c457b043a6b27b0a7cf1b7f463af956b50342
jsancs05 <javivisan19@gmail.com>
2021-10-26 15:11:13 +0200

    Update operator_test.dart

--- test/operator_test.dart
+++ test/operator_test.dart
@@ -1,19 +1,19 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:github_actions/operator.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:github_actions/operator.dart';
 
-main() {
-  group('test operations', () {
-    Operator op = Operator();
-    test('test sum function', () {
-      int a = 1;
-      int b = 6;
-      expect(op.sum(a, b), 7);
-    });
+// main() {
+//   group('test operations', () {
+//     Operator op = Operator();
+//     test('test sum function', () {
+//       int a = 1;
+//       int b = 6;
+//       expect(op.sum(a, b), 7);
+//     });
 
-    test('test sub function', () {
-      int a = 5;
-      int b = 2;
-      expect(op.sub(a, b), 3);
-    });
-  });
-}
+//     test('test sub function', () {
+//       int a = 5;
+//       int b = 2;
+//       expect(op.sub(a, b), 3);
+//     });
+//   });
+// }
